8bc1fd5730520b151b6de02e07dceeadd3a1f954
Odoo <Odoo>
2020-04-18 18:32:58 +0530

    [UPDT] Change Version Number

--- om_hospital/__manifest__.py
+++ om_hospital/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Odoo Development Tutorials',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.1.0',
     'category': 'Extra Tools',
     'summary': 'Odoo Development Tutorials For Beginners',
     'sequence': '10',
